523c32e164e6d7ab6d0fba59c9a37fee37ea1b0d
Felix Okoth Otieno <60138507+FELIXOKOTH@users.noreply.github.com>
2025-05-26 14:29:26 +0300

    Update carbon_engine.py

--- backend/carbon_engine.py
+++ backend/carbon_engine.py
@@ -3,12 +3,12 @@ def calculate_saved_trees(receipt_count:int)->float:
   """
   Calculate the estimated number of trees saved based on digital receipts.
   """
-  trees_per_paper_receipt =1/8333 # Approx.receipt per tree
-return round(receipt_count *trees_per_paper_receipt. 4)
+  trees_per_paper_receipt = 1 / 8333  # Approx. receipt per tree
+  return round(receipt_count * trees_per_paper_receipt, 4)
 
-def calculate_carbon_avaoided(recept_count:int)->float:
+def calculate_carbon_avoided(receipt_count: int) -> float:
   """
-  calculate the carbon emissions avoided by using digital receipts (in kg CO2e).
-  """ 
-carbon_per_paper_receipt=0.0004 #Example estimate
-return round(receipt_count *carbon_paper_receipt. 4)
+  Calculate the carbon emissions avoided by using digital receipts (in kg CO2e).
+  """
+  carbon_per_paper_receipt = 0.0004  # Example estimate
+  return round(receipt_count * carbon_per_paper_receipt, 4)
